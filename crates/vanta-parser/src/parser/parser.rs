451e2772a649d9b453425cf7e615867df1437066
Caio Santos <caio.francelinosena@gmail.com>
2026-04-22 20:25:04 -0400

    Add method parsing to the Parser; implement tests for class methods and parameters

--- crates/vanta-parser/src/parser/parser.rs
+++ crates/vanta-parser/src/parser/parser.rs
@@ -29,12 +29,13 @@ impl Parser {
         let fields = self.parse_fields()?;
         self.expect(TokenKind::RParen)?;
         self.expect(TokenKind::LBrace)?;
+        let methods = self.parse_methods()?;
         self.expect(TokenKind::RBrace)?;
 
         Ok(ClassDecl {
             name,
             fields,
-            methods: vec![],
+            methods,
         })
     }
 
@@ -186,6 +187,68 @@ impl Parser {
                 found: format!("{other:?}"),
             }),
         }
+    }
+
+    fn parse_methods(&mut self) -> Result<Vec<vanta_ast::FunctionDecl>, Diagnostic> {
+        let mut methods = Vec::new();
+
+        while !self.check(&TokenKind::RBrace) {
+            methods.push(self.parse_method()?);
+        }
+
+        Ok(methods)
+    }
+
+    fn parse_method(&mut self) -> Result<vanta_ast::FunctionDecl, Diagnostic> {
+        let visibility = self.parse_visibility()?;
+        self.expect(TokenKind::Function)?;
+        let name = self.expect_identifier()?;
+
+        self.expect(TokenKind::LParen)?;
+        let params = self.parse_params()?;
+        self.expect(TokenKind::RParen)?;
+
+        let return_type = if self.check(&TokenKind::Colon) {
+            self.advance();
+            Some(self.parse_type()?)
+        } else {
+            None
+        };
+
+        self.expect(TokenKind::LBrace)?;
+        self.expect(TokenKind::RBrace)?;
+
+        Ok(vanta_ast::FunctionDecl {
+            visibility,
+            name,
+            params,
+            return_type,
+            body: vec![],
+        })
+    }
+
+    fn parse_params(&mut self) -> Result<Vec<vanta_ast::Param>, Diagnostic> {
+        let mut params = Vec::new();
+
+        while !self.check(&TokenKind::RParen) {
+            let name = self.expect_identifier()?;
+            self.expect(TokenKind::Colon)?;
+            let ty = self.parse_type()?;
+
+            params.push(vanta_ast::Param { name, ty });
+
+            if self.check(&TokenKind::Comma) {
+                self.advance();
+
+                if self.check(&TokenKind::RParen) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Ok(params)
     }
 
     fn check(&self, expected: &TokenKind) -> bool {
--- crates/vanta-parser/src/parser/tests.rs
+++ crates/vanta-parser/src/parser/tests.rs
@@ -84,3 +84,56 @@ fn should_parse_fields_with_comma() {
 
     assert_eq!(class.fields.len(), 2);
 }
+
+#[test]
+fn should_parse_class_with_method() {
+    let tokens = lex(r#"
+        class User(
+            priv val name: String,
+            priv val em: String
+        ) {
+            pub function greet(): String {
+            }
+        }
+        "#)
+    .unwrap();
+
+    let mut parser = Parser::new(tokens);
+    let program = parser.parse_program().unwrap();
+
+    assert_eq!(program.classes.len(), 1);
+
+    let class = &program.classes[0];
+    assert_eq!(class.name, "User");
+    assert_eq!(class.fields.len(), 2);
+    assert_eq!(class.methods.len(), 1);
+
+    let method = &class.methods[0];
+    assert_eq!(method.name, "greet");
+    assert_eq!(method.visibility, vanta_ast::Visibility::Pub);
+    assert_eq!(method.return_type, Some(vanta_ast::Type::String));
+    assert_eq!(method.params.len(), 0);
+    assert!(method.body.is_empty());
+}
+
+#[test]
+fn should_parse_method_with_parameter() {
+    let tokens = lex(r#"
+        class User() {
+            pub function setName(value: String): Void {
+            }
+        }
+        "#)
+    .unwrap();
+
+    let mut parser = Parser::new(tokens);
+    let program = parser.parse_program().unwrap();
+
+    let method = &program.classes[0].methods[0];
+
+    assert_eq!(method.name, "setName");
+    assert_eq!(method.params.len(), 1);
+    assert_eq!(method.params[0].name, "value");
+    assert_eq!(method.params[0].ty, vanta_ast::Type::String);
+    assert_eq!(method.return_type, Some(vanta_ast::Type::Void));
+}
